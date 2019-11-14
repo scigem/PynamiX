3744232d2f0cb66ad41058eaa9a4b226f5d31738
benjym <benjy.marks@sydney.edu.au>
2019-11-14 11:45:47 +1100

    First commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pynamix", # Replace with your own username
+    version="0.0.1",
+    description='Tools for dealing with radiographs produced in DynamiX',
+    url='http://github.sydney.edu.au/scigem/pynamix',
+    author='Benjy Marks',
+    author_email='benjy.marks@sydney.edu.au',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
+        "Operating System :: OS Independent",
+        "Development Status :: 3 - Alpha",
+    ],
+    python_requires='>=3.6',
+)
