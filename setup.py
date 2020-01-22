0288389a6522c4b5f82023785b8fab57e716dfd9
Benjy Marks <benjy.marks@sydney.edu.au>
2020-01-22 20:26:09 +1100

    Version bump to indicate showstopping bugs being fixed for orientation measurement

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pynamix", # Replace with your own username
-    version="0.0.5",
+    version="0.0.6",
     description='Tools for dealing with radiographs produced in DynamiX',
     url='http://github.sydney.edu.au/scigem/pynamix',
     author='Benjy Marks, François Guillard',
