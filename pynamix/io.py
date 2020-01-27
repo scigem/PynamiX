f37d9fe61ce8fba7083f3df997e8dec1c109686e
Benjy Marks <benjy.marks@sydney.edu.au>
2020-01-28 10:45:14 +1100

    Test commit for pushing to mattermost

--- pynamix/io.py
+++ pynamix/io.py
@@ -1,4 +1,4 @@
-import os, json, glob, requests, shutil, re
+import os, json, glob, requests, shutil, re 
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm, Normalize
